4b437d2e27f2f73c7e67d8dfc10f2074d44b95db
Lilian <lilgousiou@gmail.com>
2024-12-20 06:24:35 +0200

    Imports order plugin, configuration and formatted files

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,21 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx,svg}"] },
-  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx,svg}'] },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'script' } },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
     rules: {
-      semi: "error",
-      "prefer-const": "error",
-      "react/react-in-jsx-scope": "off",
-      "react/jsx-uses-react": "off"
+      semi: 'error',
+      'prefer-const': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
     },
   },
 ];
