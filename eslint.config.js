cab61a56be758c2fd18363b0e2af66efeb22c369
Lilian <lilgousiou@gmail.com>
2024-12-20 05:35:52 +0200

    Eslint and prettier configuration and scripts

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,20 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx,svg}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      semi: "error",
+      "prefer-const": "error",
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off"
+    },
+  },
 ];
